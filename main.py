13db6dfc195635fff2c376b30ffe6e31d199d784
edos10 <riabov.edik201533@gmail.com>
2024-02-12 14:01:43 +0300

    fix to print all examples and fix magic constants

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def rec(cur_chars: list, pos: int) -> str:
+REQUIRED_CONST = 200
+START_CIPHER = 9
+
+def rec(cur_chars: list, pos: int, ans: list) -> None:
     if pos == -1:
         cur_str = ''.join(cur_chars).rstrip('+').rstrip('-')
-        if eval(cur_str) == 200:
-            return cur_str
-        return None
+        if eval(cur_str) == REQUIRED_CONST:
+            ans.append(cur_str)
+        return
     cur_chars.append(str(pos))
-    return rec(cur_chars + ['+'], pos - 1) or rec(cur_chars + ['-'], pos - 1) or rec(cur_chars, pos - 1)
+    return rec(cur_chars + ['+'], pos - 1, ans) or rec(cur_chars + ['-'], pos - 1, ans) \
+           or rec(cur_chars, pos - 1, ans)
 
-print(rec([], 9))
+
+ans = []
+rec([], START_CIPHER, ans)
+for elem in set(ans):
+    print(elem)
